c0bc921410ef884617821299009ce9b3ff96d87e
Ali Chahine <ali.chahine@okation.ai>
2023-08-02 11:33:18 +0300

    Your commit message

---
+++ New folder/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for,jsonify
+import requests
+import json
+from flask_cors import CORS
+from ultrabot import ultraChatBot
+app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
+
+
+# Load environment variables from .env file
+# os.getenv('OPENAI_API_KEY')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+def home():
+    if request.method == 'POST':
+        bot = ultraChatBot(request.json)
+        return bot.Processingـincomingـmessages()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
